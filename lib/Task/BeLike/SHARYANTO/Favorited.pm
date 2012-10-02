a78fa0f5670242c48b37937d40fc19d55eec38a9
Steven Haryanto <stevenharyanto@gmail.com>
2012-10-02 03:41:58 +0000

    Import of SHARYANTO/Task-BeLike-SHARYANTO-Favorited-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-SHARYANTO-Favorited
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-BeLike-SHARYANTO-Favorited-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/SHARYANTO/Favorited.pm
@@ -0,0 +1,174 @@
+package Task::BeLike::SHARYANTO::Favorited;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install all SHARYANTO's favorite modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::BeLike::SHARYANTO::Favorited - Install all SHARYANTO's favorite modules
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install modules favorited by SHARYANTO on L<http://metacpan.org>.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Data::Rmap>
+
+=head3 L<HTML::Escape>
+
+=head3 L<App::PPI::Dumper>
+
+=head3 L<Scalar::Util::Numeric>
+
+=head3 L<perlsecret>
+
+=head3 L<App::cpanoutdated>
+
+=head3 L<cpan::listchanges>
+
+=head3 L<App::pmuninstall>
+
+=head3 L<App::perlbrew>
+
+=head3 L<Config::IniFiles>
+
+=head3 L<Regexp::Grammars>
+
+=head3 L<Plack>
+
+=head3 L<PSGI>
+
+=head3 L<Starman>
+
+=head3 L<Class::Inspector>
+
+=head3 L<Text::ASCIITable>
+
+=head3 L<Term::ANSIColor>
+
+=head3 L<syntax>
+
+=head3 L<File::Flock>
+
+=head3 L<Module::Path>
+
+=head3 L<Module::List>
+
+=head3 L<Sort::Versions>
+
+=head3 L<Digest::MD5>
+
+=head3 L<Dist::Zilla>
+
+=head3 L<Test::Exception>
+
+=head3 L<Test::Simple>
+
+=head3 L<REST::Google>
+
+=head3 L<CHI>
+
+=head3 L<Tie::Cache>
+
+=head3 L<WWW::Mechanize>
+
+=head3 L<String::ShellQuote>
+
+=head3 L<Proc::PID::File>
+
+=head3 L<Path::Class>
+
+=head3 L<Mouse>
+
+=head3 L<Moose>
+
+=head3 L<Mojolicious>
+
+=head3 L<YAML::LibYAML>
+
+=head3 L<YAML::Syck>
+
+=head3 L<YAML>
+
+=head3 L<List::MoreUtils>
+
+=head3 L<JSON>
+
+=head3 L<LWP>
+
+=head3 L<File::Which>
+
+=head3 L<File::HomeDir>
+
+=head3 L<DBD::SQLite>
+
+=head3 L<DBI>
+
+=head3 L<autodie>
+
+=head3 L<DateTime>
+
+=head3 L<DateTime::BusinessHours>
+
+=head3 L<Moo>
+
+=head3 L<Data::Clone>
+
+=head3 L<UUID::Random>
+
+=head3 L<Capture::Tiny>
+
+=head3 L<File::LibMagic>
+
+=head3 L<Devel::Platform::Info>
+
+=head3 L<File::Slurp>
+
+=head3 L<File::chdir>
+
+=head3 L<App::cpanminus>
+
+=head3 L<Data::Printer>
+
+=head3 L<Carp::Always::Color>
+
+=head3 L<Carp::Always>
+
+=head3 L<Taint::Util>
+
+=head3 L<App::perlfind>
+
+=head3 L<PerlX::Maybe>
+
+=head3 L<Data::DPath>
+
+=head3 L<Log::Any>
+
+=head3 L<Marpa>
+
+=head1 AUTHOR
+
+SHARYANTO <SHARYANTO@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
